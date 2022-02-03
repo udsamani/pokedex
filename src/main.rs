838006c6e69bb6a72df6e0caefe77815f2d837a9
udsamani <uditsamani2@gmail.com>
2022-02-03 07:00:11 -0800

    Add logging library

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@ mod domain;
 mod repositories;
 mod errors;
 use std::sync::Arc;
+use log::info;
+use std::env;
+use env_logger;
 
 #[macro_use]
 extern crate rouille;
 
 fn main() {
+    env_logger::init();
     let repo = Arc::new(repositories::pokemon::RustemonRepository::new());
-    println!("Starting server on 8080 port");
+    info!("Starting server on 8080 port");
     api::server("0.0.0.0:8080", repo)
 }
